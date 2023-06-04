de65d469f1324dbca632756c222813fecfb7586b
NSombekke <nielssombekke@gmail.com>
2023-06-04 10:45:11 +0200

    Visualized first samples

--- src/transform.py
+++ src/transform.py
@@ -9,6 +9,8 @@ def get_transforms(input_size):
                                          transforms.Pad((1, 0)),
                                          transforms.Resize(input_size),
                                          transforms.ToTensor(),])
-    test_transform = transforms.Compose([transforms.ToTensor(),
-                                         transforms.Pad((1, 0)),])
+    test_transform = transforms.Compose([transforms.ToPILImage(),
+                                         transforms.Pad((1, 0)),
+                                         transforms.Resize(input_size),
+                                         transforms.ToTensor(),])
     return {'image': image_transform, 'mask': mask_transform, 'test': test_transform}
